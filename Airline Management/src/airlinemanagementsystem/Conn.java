436ec6134c5fa51bd1e48604a9f3ea8e5051b142
Sooraj Kumar <kkumarsooraj@gmail.com>
2024-10-18 16:58:05 +0530

    Update Conn.java

--- Airline Management/src/airlinemanagementsystem/Conn.java
+++ Airline Management/src/airlinemanagementsystem/Conn.java
@@ -10,7 +10,7 @@ public class Conn {
     public Conn() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            c = DriverManager.getConnection("jdbc:mysql:///airlinemanagementsystem", "root", "codeforinterview");
+            c = DriverManager.getConnection("jdbc:mysql:///airlinemanagementsystem", "root", "root");
             s = c.createStatement();
         } catch (Exception e) {
             e.printStackTrace();
